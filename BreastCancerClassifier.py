01824677482ecc2ddd6654079e3b33ab91cf9a74
jayaktsh <104818574+jayaktsh@users.noreply.github.com>
2022-06-12 07:45:49 +0530

    Update BreastCancerClassifier.py

--- BreastCancerClassifier.py
+++ BreastCancerClassifier.py
@@ -6,8 +6,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 # loading the data from sklearn
-breast_cancer_dataset = pd.read_csv(datasheets/BreastCancerData.csv)
-print(breast_cancer_dataset)
+breast_cancer_dataset = sklearn.datasets.load_breast_cancer()
+# print(breast_cancer_dataset)
 
 # loading the data to a data frame
 data_frame = pd.DataFrame(breast_cancer_dataset.data, columns=breast_cancer_dataset.feature_names)
@@ -33,7 +33,7 @@ data_frame.isnull().sum()
 # statistical measures about the data
 data_frame.describe()
 
-# checking the distribution of Target Varibale
+# checking the distribution of Target Variable
 data_frame['label'].value_counts()
 
 data_frame.groupby('label').mean()
@@ -41,11 +41,11 @@ data_frame.groupby('label').mean()
 X = data_frame.drop(columns='label', axis=1)
 Y = data_frame['label']
 
-print(X)
-print(Y)
+# print(X)
+# print(Y)
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
-print(X.shape, X_train.shape, X_test.shape)
+# print(X.shape, X_train.shape, X_test.shape)
 
 model = LogisticRegression()
 
@@ -56,28 +56,27 @@ model.fit(X_train, Y_train)
 X_train_prediction = model.predict(X_train)
 training_data_accuracy = accuracy_score(Y_train, X_train_prediction)
 
-print('Accuracy on training data = ', training_data_accuracy)
+# print('Accuracy on training data = ', training_data_accuracy)
 
 # accuracy on test data
 X_test_prediction = model.predict(X_test)
 test_data_accuracy = accuracy_score(Y_test, X_test_prediction)
 
-print('Accuracy on test data = ', test_data_accuracy)
+# print('Accuracy on test data = ', test_data_accuracy)
 
-input_data = (13.54,14.36,87.46,566.3,0.09779,0.08129,0.06664,0.04781,0.1885,0.05766,0.2699,0.7886,2.058,23.56,0.008462,0.0146,0.02387,0.01315,0.0198,0.0023,15.11,19.26,99.7,711.2,0.144,0.1773,0.239,0.1288,0.2977,0.07259)
+input_data = (13.54, 14.36, 87.46, 566.3, 0.09779, 0.08129, 0.06664, 0.04781, 0.1885, 0.05766, 0.2699, 0.7886, 2.058, 23.56, 0.008462, 0.0146, 0.02387, 0.01315, 0.0198, 0.0023, 15.11, 19.26, 99.7, 711.2, 0.144, 0.1773, 0.239, 0.1288, 0.2977, 0.07259)
 
 # change the input data to a numpy array
 input_data_as_numpy_array = np.asarray(input_data)
 
 # reshape the numpy array as we are predicting for one datapoint
-input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
 prediction = model.predict(input_data_reshaped)
-print(prediction)
+# print(prediction)
 
 if (prediction[0] == 0):
-  print('The Breast cancer is Malignant')
+    print('The Breast cancer is Malignant')
 
 else:
-  print('The Breast Cancer is Benign')
-
+    print('The Breast Cancer is Benign')
